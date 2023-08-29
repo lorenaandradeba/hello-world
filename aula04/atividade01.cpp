ec061663263cab673ba15fe6adc1e3f2128598e6
Lorena Andrade <lorenaandrade@gmail.com>
2023-08-29 11:47:44 -0300

    commit3 da aula 04

---
+++ aula04/atividade01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    cout << "'0' = " << int('0') << endl;
+    cout << "'1' = " << int('1') << endl;
+    cout << "'2' = " << int('2') << endl;
+    cout << "'3' = " << int('3') << endl;
+    cout << "'4' = " << int('4') << endl;
+    cout << "'5' = " << int('5') << endl;
+    cout << "'6' = " << int('6') << endl;
+    cout << "'7' = " << int('7') << endl;
+    cout << "'8' = " << int('8') << endl;
+    cout << "'9' = " << int('9') << endl;
+    return 0;
+}
